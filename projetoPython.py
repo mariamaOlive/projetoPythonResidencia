978f48a267ce5109fb6558767eab5c212a85b655
lucascouri2 <lncc2@cin.ufpe.br>
2021-09-24 18:34:14 -0300

    Update projetoPython.py

--- projetoPython.py
+++ projetoPython.py
@@ -1,4 +1,27 @@
 ### Projeto Residencia Python - Operadoras Telefonicas
 ### Lucas e Mariama
 
-print("Hello world")
+def listarClientes(dicionarioClientes):
+    ###print
+
+def inserirCliente(dicionarioClientes, cliente)
+    ###
+    return Bool
+    
+def receberInfoCliente(infoCliente) ### pega os inputs para usar em inserirCliente
+    ### recebe input com as informações do cliente
+    return cliente
+
+def buscarCliente(dicionarioClientes, CPF):
+    ###
+    return cliente
+
+def atualizaSaldo(dicionarioClientes, CPF, novoSaldo):
+    ###
+    return Bool
+
+def atualizaChamada(dicionarioClientes, CPF, chamada):
+    return Bool
+
+def deletarCliente(dicionarioClientes, CPF):
+    ###
